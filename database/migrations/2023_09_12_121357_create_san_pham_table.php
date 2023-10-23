1d9f77aae30b62a9251bd3524bb09d02d217b5f6
plai201 <nguyenthanhtungksa@gmail.com>
2023-10-24 03:20:53 +0700

    update

--- database/migrations/2023_09_12_121357_create_san_pham_table.php
+++ database/migrations/2023_09_12_121357_create_san_pham_table.php
@@ -17,21 +17,21 @@ class CreateSanphamTable extends Migration
 
             $table->bigIncrements('MaSanPham')->comment('Mã sản phẩm');
             $table->string('TenSanPham', 191)->comment('Tên sản phẩm # Tên sản phẩm');
-            $table->unsignedInteger('GiaGoc')->default('0')->comment('Giá gốc # Giá gốc của sản phẩm');
+            $table->float('GiaGoc')->default('0')->comment('Giá gốc # Giá gốc của sản phẩm');
 
-            $table->unsignedInteger('GiamGia')->default('0')->comment('Giảm giá #Phần trăm giảm giá bán sản phẩm');
-            $table->unsignedInteger('GiaBan')->default('0')->comment('Giá bán # Giá bán hiện tại của sản phẩm');
-            $table->unsignedInteger('TraGop')->default('0')->comment('Trả góp');
+            $table->integer('GiamGia')->default('0')->comment('Giảm giá #Phần trăm giảm giá bán sản phẩm');
+            $table->float('GiaBan')->default('0')->comment('Giá bán # Giá bán hiện tại của sản phẩm');
+            $table->integer('TraGop')->default('0')->comment('Trả góp');
             $table->string('AnhSanPham', 255)->comment('Hình đại diện # Hình đại diện của sản phẩm');
             $table->string('TenAnh', 255)->comment('Hình đại diện # Hình đại diện của sản phẩm');
             $table->text('MoTaSanPham')->comment('Mô tả # Mô tả về sản phẩm');
 
 
-            $table->tinyInteger('TrangThai')->default('2')->comment('Trạng thái # Trạng thái sản phẩm: 1-khóa, 2-khả dụng');
-            $table->unsignedTinyInteger('MaDanhMuc')->comment('Loại sản phẩm # Mã danh muc san pham');
+            $table->integer('TrangThai')->default('2')->comment('Trạng thái # Trạng thái sản phẩm: 1-khóa, 2-khả dụng');
+            $table->integer('MaDanhMuc')->comment('Loại sản phẩm # Mã danh muc san pham');
 
-            $table->unsignedTinyInteger('MataiKhoan')->comment('Tài khoản # Mã tài khoản');
-            $table->unsignedTinyInteger('MaNhanHang')->comment('Nhãn hàng # Mã nhãn hàng, thuương hiệu');
+            $table->integer('MataiKhoan')->comment('Tài khoản # Mã tài khoản');
+            $table->integer('MaNhanHang')->comment('Nhãn hàng # Mã nhãn hàng, thuương hiệu');
             $table->integer('LuotXem')->default('0');
             $table->integer('SanPhamSoLuong')->default('0');
             $table->integer('SanPhamBan')->default('0');
